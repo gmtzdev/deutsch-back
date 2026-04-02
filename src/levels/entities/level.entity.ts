4aab0325e2c63ee666b589bb2c102df8163f0dc1
gmtzdev <gerardomtzisc@gmail.com>
2026-04-02 13:47:00 -0600

    ✨ Define Level entity with properties and TypeORM decorators

--- src/levels/entities/level.entity.ts
+++ src/levels/entities/level.entity.ts
@@ -1,1 +1,22 @@
-export class Level {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Level {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    icon: string;
+
+    @Column()
+    tag: string;
+
+    @Column({ default: 0 })
+    lessonNumber: number;
+}
