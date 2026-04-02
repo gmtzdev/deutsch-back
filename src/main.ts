3cffba8a403fc82a0ec368da0f88f001a51d20b3
gmtzdev <gerardomtzisc@gmail.com>
2026-04-02 13:42:14 -0600

    ✨ Add Swagger setup for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+/* Swagger setup */
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Deutsch API')
+    .setDescription('The Deutsch API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
