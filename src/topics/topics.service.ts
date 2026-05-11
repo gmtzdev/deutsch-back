9b4939cb842c2d564962ebb00ef2d28ee72abcda
gmtzdev <gerardomtzisc@gmail.com>
2026-05-11 09:54:54 -0600

    🐛 fix(topics): update method implementation to handle topic updates and error handling

--- src/topics/topics.service.ts
+++ src/topics/topics.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateTopicDto } from './dto/create-topic.dto';
 import { UpdateTopicDto } from './dto/update-topic.dto';
 import { Topic } from './entities/topic.entity';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LevelsService } from 'src/levels/levels.service';
 
@@ -29,8 +29,12 @@ export class TopicsService {
 
   }
 
-  update(id: number, updateTopicDto: UpdateTopicDto) {
-    return `This action updates a #${id} topic`;
+  async update(id: number, updateTopicDto: UpdateTopicDto) {
+    const result: UpdateResult = await this.topicRepository.update(id, updateTopicDto);
+    if (result.affected === 0) {
+      throw new Error(`Topic with id ${id} not found`);
+    }
+    return this.topicRepository.findOneBy({ id });
   }
 
   remove(id: number) {
